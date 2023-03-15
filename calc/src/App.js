afb15063a435db5def1205b0d9318f769ab4ed47
Svetlana Trukhina <sweta4ca@mail.ru>
2023-03-15 22:04:13 +0700

    feat: add base layout

---
+++ calc/src/App.js
@@ -0,0 +1,12 @@
+import { Calculator } from "./components/Calculator";
+
+function App() {
+  return (
+    <div>
+      <h1>hello from App.js!</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ calc/src/components/Runtime.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Runtime.sass";
+
+export const Runtime = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="title">Runtime component!</h1>
+    </div>
+  );
+};
